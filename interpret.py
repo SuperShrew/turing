12b63d1fee12482d8e0f8611a097ddc32fbe716d
SuperShrew <150274640+SuperShrew@users.noreply.github.com>
2025-01-28 08:49:10 +0000

    chang

--- interpret.py
+++ interpret.py
@@ -1,8 +1,18 @@
-with open("main.txt", "r") as f:
-  data = f.readlines()
-  if data[0][0] == "!":
-    memory = [0]*int(data[0][1:])
-    print(memory)
-    run = True
-  else:
-    print("Error, initialiser (!) was not found")
+def interpret(file):
+  with open(file, "r") as f:
+    data = f.readlines()
+    if data[0][0] == "!":
+      memory = [0]*int(data[0][1:])
+      pointer = 0
+      print(memory)
+      run = True
+    else:
+      print("Error, initialiser (!) was not found")
+      exit()
+  
+    for line in data:
+      line = line.split()
+      if line[0] == "SET":
+        print("Sett")
+
+interpret("main.txt")
